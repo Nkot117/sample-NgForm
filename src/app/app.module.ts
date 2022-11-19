d1c7074b0d6263a72e2c8cde5e678b810e368bc5
Naoto Kumakura <kumakura117@gmail.com>
2022-11-20 00:02:35 +0900

    ・デザインの修正 ・ユーザー情報を入力するフォームをコンポーネント化 ・Bootstrapを導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, UserFormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })

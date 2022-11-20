4145ee731575dacd596cd7e3e3af8e42455e8c9b
Naoto Kumakura <kumakura117@gmail.com>
2022-11-20 10:44:35 +0900

    ・フォームの実装

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-form',
@@ -6,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  public user: User = new User("","")
+  public showErrorMessage: boolean = false;
   constructor() {}
-
   ngOnInit(): void {
     console.log('UserFormComponent@ngOnInit');
+  }
+
+  public submit(form: NgForm): void{
+    alert(`フォームの入力値 ：${JSON.stringify(form.value)}`)
+
+    if(form.invalid){
+      this.showErrorMessage = true;
+      return;
+    }
+
+    form.reset()
+    this.showErrorMessage = false;
   }
 }
